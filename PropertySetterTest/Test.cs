223c4a8bac0ddf9ea38b21f4d5e640d8bffe7631
Derek Foulk <dfoulk@colheli.com>
2018-06-28 16:22:16 -0700

    Removed embedded instructions

--- PropertySetterTest/Test.cs
+++ PropertySetterTest/Test.cs
@@ -20,16 +20,7 @@ namespace PropertySetterTest
                 if (value == _networkCredential)
                     return;
 
-                // INSERT BREAKPOINT HERE and follow these steps:
-                // 1) Once in place: Run the app and let it hit this breakpoint
-                // 2) Once this breakpoint is hit: Hit F10 once to step to the next line
-                // 3) IMPORTANT: Examine 'value' and 'this > _networkCredential' in the 'Locals' tab (_networkCredential != value for some reason?)
-                // 4) Hit 'Continue' to finish executing the app
-                // 5) Notice that 'Program.RunTest() > Child.Test()' returns 'false'; indicating a test failure
-                // 6) Disable all breakpoints
-                // 7) Run the app again
-                // 8) Notice that 'Program.RunTest() > Child.Test()' returns 'true'; indicating a test success
-                // 9) Try to wipe the confused look off your face
+                // INSERT BREAKPOINT HERE
                 _networkCredential = value;
 
                 Storage.NetworkCredential = _networkCredential;
