1891528a19b44f39210b551033851c41537cb311
Derek Foulk <dfoulk@colheli.com>
2018-06-28 12:44:29 -0700

    Add project files.

---
+++ PropertySetterTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PropertySetterTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
